1447057e95eed63cee29cc702b3d73807161d386
malikaishacc00-alt <malikaishacc00@gmail.com>
2026-08-13 08:25:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 import json
+import qrcode
+import io
 
 app = Flask(__name__)
 
 
+def load_documents():
+    with open("documents.json") as file:
+        return json.load(file)
+
+
 @app.route("/")
 def verification():
-    with open("documents.json") as file:
-        documents = json.load(file)
-
-    document = documents["DOC-2026-0001"]
+    documents = load_documents()
+    document_id = "DOC-2026-0001"
+    document = documents[document_id]
 
     return render_template(
         "verify.html",
-        document=document
+        document=document,
+        document_id=document_id
     )
+
+
+@app.route("/verify/<document_id>")
+def verify_document(document_id):
+    documents = load_documents()
+
+    if document_id not in documents:
+        return "INVALID DOCUMENT", 404
+
+    document = documents[document_id]
+
+    return render_template(
+        "verify.html",
+        document=document,
+        document_id=document_id
+    )
+
+
+@app.route("/qr/<document_id>")
+def generate_qr(document_id):
+    url = f"https://qr-verification-system-5er6.onrender.com/verify/{document_id}"
+
+    qr = qrcode.make(url)
+
+    img = io.BytesIO()
+    qr.save(img, format="PNG")
+    img.seek(0)
+
+    return send_file(img, mimetype="image/png")
 
 
 if __name__ == "__main__":
